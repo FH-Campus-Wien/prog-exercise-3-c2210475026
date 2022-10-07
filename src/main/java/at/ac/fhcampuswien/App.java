2b9423d3936d126cc415f4aa55144deaca508e01
alexandermessner <alexander.messner1@stud.fh-campuswien.ac.at>
2022-10-08 01:03:05 +0200

    Task 1 complete

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,21 +6,20 @@ import java.util.Scanner;
 public class App {
 
     public static void oneMonthCalendar(int tage,int firstday){
-        double zeilen=(tage+firstday)/7;
+        boolean loop =true;
         int tag=1;
-        for(int i =1;i<=zeilen;i++){
-            if(i==1){
-                for(int o=1;o<firstday;o++){
-                    System.out.print("   ");
-                }
-                firstday=firstday-1;
-            }
+        for(int o=1;o<firstday;o++){
+            System.out.print("   ");
+        }
+        firstday=firstday-1;
+        while(loop){
             for(int s=1+firstday;s<=7;s++){
                 if(tag<=9 && tag>=1){
                     System.out.print(" "+tag+" ");
                 } else {
                     System.out.print(tag+" ");
                     if(tag==tage){
+                        loop=false;
                         break;
                     }
                 }
