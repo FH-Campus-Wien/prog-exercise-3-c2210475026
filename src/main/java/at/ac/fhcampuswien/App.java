5064a51c89766339ff67ad6f82d236c2bbda5a60
alexandermessner <alexander.messner1@stud.fh-campuswien.ac.at>
2022-10-08 01:00:58 +0200

    Task 1 almost done

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -5,7 +5,31 @@ import java.util.Scanner;
 
 public class App {
 
-    // Implement all methods as public static
+    public static void oneMonthCalendar(int tage,int firstday){
+        double zeilen=(tage+firstday)/7;
+        int tag=1;
+        for(int i =1;i<=zeilen;i++){
+            if(i==1){
+                for(int o=1;o<firstday;o++){
+                    System.out.print("   ");
+                }
+                firstday=firstday-1;
+            }
+            for(int s=1+firstday;s<=7;s++){
+                if(tag<=9 && tag>=1){
+                    System.out.print(" "+tag+" ");
+                } else {
+                    System.out.print(tag+" ");
+                    if(tag==tage){
+                        break;
+                    }
+                }
+                tag=tag+1;
+                firstday=0;
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
         // test your method implementations here
